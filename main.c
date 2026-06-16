3e193a0de0e421e2797006b96a31f3b8a0a82ce1
rapon1kt <shinnatensei@gmail.com>
2026-06-16 11:02:07 -0300

    feat: adicionando arquivos c e funcoes (insercao e busca)

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "patricia.h"
+
+int main(int argc, char ** argv) {
+  printf("Blablabla");
+  return 0;
+}
---
+++ patricia.h
@@ -0,0 +1,16 @@
+#ifndef PATRICIA_H
+#define PATRICIA_H
+
+typedef struct PatriciaNode PatriciaNode;
+
+PatriciaNode *criar_nodo(const char *texto);
+
+int maiorPrefixoEmComum(const char *a, const char *b);
+
+void inserir(PatriciaNode **raiz, const char *palavra);
+int buscar(PatriciaNode *raiz, const char *palavra);
+
+void imprimir(PatriciaNode *raiz, int nivel);
+void destruir(PatriciaNode *raiz);
+
+#endif
