152c371259770a25ed59102fc4c3a8918d4de918
rapon1kt <shinnatensei@gmail.com>
2026-06-19 17:50:44 -0300

    refactor: removing wrong version of patricia trie

--- main.c
+++ main.c
@@ -1,29 +1,6 @@
-#include "patricia.h"
 #include <stdio.h>
 
 int main() {
-
-  PatriciaNode *raiz = NULL;
-
-  inserir(&raiz, "casa");
-  inserir(&raiz, "caso");
-  inserir(&raiz, "cas");
-  inserir(&raiz, "cama");
-  inserir(&raiz, "carro");
-  inserir(&raiz, "cachorro");
-  inserir(&raiz, "mesa");
-
-  printf("ARVORE:\n\n");
-
-  imprimir(raiz, 0);
-
-  printf("\n");
-
-  printf("casa: %d\n", buscar(raiz, "casa"));
-  printf("caso: %d\n", buscar(raiz, "caso"));
-  printf("cas: %d\n", buscar(raiz, "cas"));
-  printf("mesa: %d\n", buscar(raiz, "mesa"));
-  printf("banana: %d\n", buscar(raiz, "banana"));
-  liberar_trie(raiz);
+  printf("Patricia Trie - Morrison\n");
   return 0;
 }
